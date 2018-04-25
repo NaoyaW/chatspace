21c4d197d96e54876538767ce93461ea3e3306bc
Naoya Watanabe <naoya@NaoyanoMacBook-Pro.local>
2018-04-25 22:01:23 +0900

    モデル作成時不要ファイルブロック

--- config/application.rb
+++ config/application.rb
@@ -13,4 +13,8 @@ module DataBaseDesignSample
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+  config.generators do |g|
+    g.assets     false
+    g.helper     false
+  end
 end
