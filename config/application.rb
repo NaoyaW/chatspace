b267217ff41c1a2a0619a4717c072456b053d841
Naoya Watanabe <naoya@NaoyanoMacBook-Pro.local>
2018-04-26 18:25:02 +0900

    不要ファイル生成ブロック機能エラーの修正

--- config/application.rb
+++ config/application.rb
@@ -6,15 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DataBaseDesignSample
+module Chatspace
   class Application < Rails::Application
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
-  config.generators do |g|
-    g.assets     false
-    g.helper     false
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
   end
 end
